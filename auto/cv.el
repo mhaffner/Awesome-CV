4c2daed27378e58d89e7ba99daf11bc66ea4dff0
Matthew Haffner <haffner.matthew.m@gmail.com>
2018-02-15 13:04:18 -0600

    Massive rework based on CV used for job applications

---
+++ auto/cv.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "cv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("awesome-cv" "12pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "cv/education"
+    "cv/publication"
+    "cv/presentation"
+    "cv/experience"
+    "cv/invited-lecture"
+    "cv/honors"
+    "cv/memberships"
+    "cv/professional-service"
+    "cv/technical-skills"
+    "awesome-cv"
+    "awesome-cv12"))
+ :latex)
+
