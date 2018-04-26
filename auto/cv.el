656845918e4bcb1638adfaf67b1c78fd76d8c15f
Matthew Haffner <haffner.matthew.m@gmail.com>
2018-04-26 12:16:17 -0500

    Massive changes for new Eau Claire CV

--- auto/cv.el
+++ auto/cv.el
@@ -3,6 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("awesome-cv" "12pt" "a4paper")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "cv/education"
@@ -16,6 +23,8 @@
     "cv/professional-service"
     "cv/technical-skills"
     "awesome-cv"
-    "awesome-cv12"))
+    "awesome-cv12")
+   (LaTeX-add-xcolor-definecolors
+    "awesome"))
  :latex)
 
