b7dcb316ef2fc8abfcd0090cb3188d9eb7c96388
Matt Haffner <haffner.matthew.m@gmail.com>
2019-02-17 13:08:19 -0600

    Add new section

--- auto/cv.el
+++ auto/cv.el
@@ -3,24 +3,25 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("awesome-cv" "12pt" "a4paper")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "cv/professional-experience"
     "cv/education"
     "cv/publication"
     "cv/presentation"
     "cv/teaching-experience"
-    "cv/other-experience"
     "cv/invited-lecture"
     "cv/honors"
     "cv/memberships"
     "cv/professional-service"
+    "cv/professional-development"
     "cv/technical-skills"
     "awesome-cv"
     "awesome-cv12")
